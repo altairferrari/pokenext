534b1e5f4d215f6c88d62e047440b2568fbc6605
Altair Ferrari Moraes <altair@GAZMGTCNB159869.gazin.com.br>
2023-06-24 23:04:07 -0300

    Enviando projeto

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Footer.module.css'
+
+export default function Footer(){
+    return (
+        <footer className={styles.footer}>
+            <p><spam>PokeNext</spam> &copy; 2023</p>
+        </footer>
+    )
+}
